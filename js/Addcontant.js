8aeac64f0de3899c6c6d136efc968d1dffa9eeeb
salehmmasri <salehalmasri494@yahoo.com>
2020-04-15 15:01:10 +0300

    use functions

--- js/Addcontant.js
+++ js/Addcontant.js
@@ -1,16 +1,26 @@
 
 var name=prompt("please enter your name");
-var salary=prompt("please enter your salary");
-if (salary<=100){
-    document.write('<h2>'+" you cant bay any thig right now "+name+'</h2>');
+var salary=prompt("please enter your salary"); 
+
+var git= function () { 
+    var value;
+if (salary>=100){
+    value='<h2>'+" you cant bay any thig right now "+name+'</h2>';
 
 }
-else if (salary >= 100){
-    document.write('<h2>'+" now you can bay "+name+'</h2>');
+else if (salary <= 100){
+     value=" now you can bay "+name;
 }
 
 else{
-    document.write('<h2>'+ "please enter number btween 100 -1000"+'</h2>');
+    value="please enter number btween 100 -1000";
 }
-alert("welcome "+name);
-confirm("show your resalt at the end of the page");
+console.log(value);
+return value;
+
+}
+document.write('<h2>'+ git() + '</h2>');
+console.log(git());
+
+
+            
